f4a3c89d57a0c30333a0fc5515c8861f7d09f361
JongOne <cowhddnjsdl0@naver.com>
2022-08-14 21:59:48 +0900

    작업 내역 호스트 댓글 구역 뷰 완성
    다음 작업 내역
    게스트 댓글 뷰 작성

--- 20questions/src/components/Ex.jsx
+++ 20questions/src/components/Ex.jsx
@@ -15,7 +15,7 @@ const Ex = () => {
             }
         })
     }, []);
-    
+
     return (
         <>
             {/* <div className="게스트구역">
@@ -40,8 +40,8 @@ const Ex = () => {
                     {comments.map((comment) => (
                         <div key={comment.count}>
                             {comments.length === 0 ? '' :
-                                <div> 
-                                    {comment.solved === null ? <div>{comment.content}<button>O</button><button>X</button></div> : <div>{comment.content}{comment.solved === true ? <p>O</p> : <p>X</p> }</div> }
+                                <div>
+                                    {comment.solved === null ? <div><Checkdiv><div>{comment.content}</div><p><button>O</button><button>X</button></p></Checkdiv></div> : <div>{comment.solved === true ? <DoneCheckdiv><div>{comment.content}</div><p><button>O</button></p></DoneCheckdiv> : <DoneCheckdiv>{comment.content}<p><button>O</button></p></DoneCheckdiv>}</div>}
                                 </div>}
                         </div>
                     ))}
@@ -58,13 +58,71 @@ export default Ex;
 let GuestBody = styled.div`
     background-color: red; 
     width: 600px;
-    height: 300px;
+    height: 500px;
     margin: auto;
 `
 
 let HostBody = styled.div`
-    background-color: purple; 
-    width: 600px;
-    height: 300px;
+    background-color: #80008018; 
+    min-width: 45%;
+    max-width: 60%;
+    height: 400px;
     margin: auto;
+    margin-top: 5px;
+    border-radius: 10px;
+    border: solid 3px black;
+    padding: 10px;
+    overflow-y: scroll;
+    div {
+        overflow: hidden;
+        white-space: nowrap;
+        width: 90%;
+        display: flex;
+        margin: auto;
+        margin-top: 5px;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+    }
+    button {
+        margin: auto 0 0 auto;
+        height: 25px;
+        display : flex;
+        justify-content : center;
+        align-items : center;
+        
+        min-width: 60px;
+        margin-right: 3px;
+        margin-top: 20px;
+        padding: 3px;
+        border: 1px solid #ff00668d;
+        border-radius: 4px;
+        background-color: #ff00668a;
+        color: white;
+    }
+    p {
+        margin: auto;
+        display: flex;
+        float: right;
+    }
+`
+
+const Checkdiv = styled.div`
+    min-width: 215px;
+    height: 100px;
+    border-radius: 10px;
+    border: solid 2px black;
+    padding: 5px;
+    text-overflow: ellipsis;
+    white-space: normal;
+`
+
+const DoneCheckdiv = styled.div`
+    min-width: 215px;
+    height: 100px;
+    border-radius: 10px;
+    border: solid 2px black;
+    padding: 5px;
+    text-overflow: ellipsis;
+    white-space: normal;
 `
