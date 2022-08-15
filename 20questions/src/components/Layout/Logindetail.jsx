8204597df3a7e6fc06ac5554f22343147f9d479a
yeun38 <choh1997@naver.com>
2022-08-15 21:42:57 +0900

    data

--- 20questions/src/components/Layout/Logindetail.jsx
+++ 20questions/src/components/Layout/Logindetail.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { useState } from "react";
-import axios from "axios";
+// import axios from "axios";
 import styled from "styled-components";
 
 function LoginLayout() {
